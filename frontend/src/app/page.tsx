834b4028772f3ba25c87551ebd23c8fcac85a5b1
Jaka Krajnc <444.ous@gmail.com>
2024-09-26 11:43:25 +0200

    feat: add tabs to flashcard console

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,7 @@
 import Link from 'next/link'
 
+// Something like this: https://ui.shadcn.com/examples/tasks
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
--- frontend/src/components/containers/console.tsx
+++ frontend/src/components/containers/console.tsx
@@ -1,8 +1,9 @@
 'use client'
 
 import { Card } from '@components/ui/card'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@components/ui/tabs'
 import { cn } from '@lib/utils'
-import { BadgeMinus, BadgePlus } from 'lucide-react'
+import { BadgeMinus, BadgePlus, Bot, Club, FileText } from 'lucide-react'
 import { useState } from 'react'
 
 type Flashcard = {
@@ -50,44 +51,70 @@ const Console = ({ chatId }: { chatId: string }) => {
 
   return (
     <div className="flex-shrink-0 space-y-3 bg-gray-300 p-4">
-      <h2 className="font-mono text-2xl">Console</h2>
-      <h4
-        className={cn(
-          isConnected ? 'text-green-700' : 'text-red-700',
-          'text-xl font-mono'
-        )}
-      >
-        {chatId}
-      </h4>
-      {flashcards.map((element) => (
-        <Card className="p-4 bg-slate-50 max-w-[25vw] relative">
-          <div className="flex space-x-1 absolute top-2 right-2">
-            <button className="active:animate-ping">
-              <BadgeMinus
-                className="hover:fill-red-300 hover:stroke-red-800"
-                size={18}
-                strokeWidth={'1.25px'}
-              />
-            </button>
-            <button className="active:animate-ping">
-              <BadgePlus
-                className="hover:fill-green-300 hover:stroke-green-800 "
-                size={18}
-                strokeWidth={'1.25px'}
-              />
-            </button>
-          </div>
+      <Tabs defaultValue="documents" className="h-full space-y-6">
+        <div className="space-between flex items-center">
+          <TabsList>
+            <TabsTrigger value="documents" className="space-x-2">
+              <FileText size={18} />
+              <p>Documents</p>
+            </TabsTrigger>
+            <TabsTrigger value="flashcards" className="space-x-2">
+              <Club size={18} />
+              <p>Flashcards</p>
+            </TabsTrigger>
+            <TabsTrigger value="flashcard-creator" className="space-x-2">
+              <Bot size={18} />
+              <p>Creator</p>
+            </TabsTrigger>
+          </TabsList>
+        </div>
+        <h2 className="font-mono text-2xl">Console</h2>
+        <h4
+          className={cn(
+            isConnected ? 'text-green-700' : 'text-red-700',
+            'text-xl font-mono'
+          )}
+        >
+          {chatId}
+        </h4>
+        <TabsContent value="documents">
+          The retrieved docs will be shown here
+        </TabsContent>
+        <TabsContent value="flashcards">
+          {flashcards.map((element) => (
+            <Card className="p-4 bg-slate-50 max-w-[25vw] relative">
+              <div className="flex space-x-1 absolute top-2 right-2">
+                <button className="active:animate-ping">
+                  <BadgeMinus
+                    className="hover:fill-red-300 hover:stroke-red-800"
+                    size={18}
+                    strokeWidth={'1.25px'}
+                  />
+                </button>
+                <button className="active:animate-ping">
+                  <BadgePlus
+                    className="hover:fill-green-300 hover:stroke-green-800 "
+                    size={18}
+                    strokeWidth={'1.25px'}
+                  />
+                </button>
+              </div>
 
-          <p className="text-slate-500 text-sm">front:</p>
-          <Card className="p-4">
-            <p key={element.id}>{element.front}</p>
-          </Card>
-          <p className="text-slate-500 text-sm">back:</p>
-          <Card className="p-4">
-            <p key={element.id}>{element.back}</p>
-          </Card>
-        </Card>
-      ))}
+              <p className="text-slate-500 text-sm">front:</p>
+              <Card className="p-4">
+                <p key={element.id}>{element.front}</p>
+              </Card>
+              <p className="text-slate-500 text-sm">back:</p>
+              <Card className="p-4">
+                <p key={element.id}>{element.back}</p>
+              </Card>
+            </Card>
+          ))}
+        </TabsContent>
+        <TabsContent value="flashcard-creator">
+          Give instructions to the flashcard creator.
+        </TabsContent>
+      </Tabs>
     </div>
   )
 }
