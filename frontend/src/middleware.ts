4e0236d4526ef65e884fd8248357dae6ce4d5cc4
Jaka Krajnc <444.ous@gmail.com>
2025-05-12 22:36:29 +0200

    🎱 expand middleware matcher pattern to exclude backend calls

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -26,6 +26,6 @@ export async function middleware(req: NextRequest) {
 export const config = {
   matcher: [
     '/',
-    '/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)'
+    '/((?!api|auth|docs|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)'
   ]
 }
