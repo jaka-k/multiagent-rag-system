b85aa64e64a71fc8466c8483d6e0acf124fc6877
Jaka Krajnc <444.ous@gmail.com>
2025-05-20 21:19:10 +0200

    👩‍🏭 clean-up flashcards controller after debugging

--- backend/server/controller/flashcards_controller.py
+++ backend/server/controller/flashcards_controller.py
@@ -1,12 +1,12 @@
 from typing import Dict
 import asyncio
 
+from server.core.logger import app_logger
 
 queues: Dict[str, asyncio.Queue] = {}
 
 ## each client session has a custom queue generator and consumer
 def get_queue_by_id(id: str):
-    print("get_queue_by_id(id: str):", id)
     if id not in queues:
         queues[id] = asyncio.Queue()
     return queues[id]
@@ -16,16 +16,13 @@ def get_queue_by_id(id: str):
 ## flashcards that are fetched with the bulk method should not be in the queue
 
 async def flashcard_sse_generator(queue: asyncio.Queue):
-    print("Started event generator")
+    app_logger.info("Started event generator")
     while True:
         flashcard_id = await queue.get()
-        print("THIS ONE IS NOT TRIGGERED", flashcard_id)
+
         yield f"event: flashcards_update\ndata: {flashcard_id}\n\n"
 
 
 async def notify_sse_client(flashcard_queue_id: str, flashcard_id: str):
     queue = get_queue_by_id(flashcard_queue_id)
-    print("THIS ONE IS TRIGGERED", flashcard_id)
-    print("QUEUE", queue)
-    print("QUEUEs", queues)
     await queue.put(flashcard_id)
